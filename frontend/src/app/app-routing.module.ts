1aa9fe221363ac8632ef1a0bb0a77c5038d620c7
ndtracer <tracejanota@gmail.com>
2024-04-25 19:28:23 -0500

    Landing/Results Init HTML

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { ResultsPageComponent } from './features/results-page/results-page.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { VerifyComponent } from './features/auth/verify/verify.component';
 
 
 const routes: Routes = [
@@ -14,6 +17,19 @@ const routes: Routes = [
     path: "login",
     component: LoginComponent
   },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "verify",
+    component: VerifyComponent
+  },
+  {
+    path: "results",
+    component: ResultsPageComponent
+  },
+
 
 ];
 
