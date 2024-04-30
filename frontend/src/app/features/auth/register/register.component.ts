b77017be750b0da9d398d2aa7ce95c9fe36e0fa6
Blake Graviett <blake.graviett@gmail.com>
2024-04-29 20:03:51 -0500

    FEAT: Finish Auth

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,53 +1,58 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
-import { AuthService } from "../../../shared/services/auth.service";
-import { Subscription } from "rxjs";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../../shared/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
-	selector: "app-register",
-	templateUrl: "./register.component.html",
-	styleUrl: "./register.component.scss"
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-	// CREATE LOGIN FORM
-	registerForm = new FormGroup({
-    firstName: new FormControl("", [Validators.required]),
-    middleInitial: new FormControl("", [Validators.required]),
-    lastName: new FormControl("", [Validators.required]),
-    phoneNumber: new FormControl("", [Validators.required]),
-    role: new FormControl("", [Validators.required]),
-		username: new FormControl("", [Validators.required]),
-		email: new FormControl("", [Validators.required, Validators.email]), // make sure it is a valid email address
-		password: new FormControl("", [Validators.required])
-	});
+  // CREATE LOGIN FORM
+  registerForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    middleInitial: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required]),
+    role: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]), // make sure it is a valid email address
+    password: new FormControl('', [Validators.required]),
+  });
 
-	private authSubscription = new Subscription();
+  private authSubscription = new Subscription();
 
-	constructor(
-		private formBuilder: FormBuilder,
-		private authService: AuthService,
-		private router: Router
-	) {}
-	// On Submit Function
-	onSubmit() {
-		if (this.registerForm.invalid) return;
-		const formValue = this.registerForm.getRawValue();
+  constructor(private authService: AuthService) {}
+  // On Submit Function
+  onSubmit() {
+    if (this.registerForm.invalid) return;
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.value.phoneNumber);
 
-    console.log(formValue)
+    this.authSubscription.add(
+      this.authService
+        .registerUser(
+          this.registerForm.value.firstName,
+          this.registerForm.value.middleInitial,
+          this.registerForm.value.lastName,
+          this.registerForm.value.email,
+          this.registerForm.value.password,
+          this.registerForm.value.role,
+          this.registerForm.value.phoneNumber
+        )
+        .subscribe(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.error('Error:', error); // Handle error
+          }
+        )
+    );
+  }
 
-		if (!formValue) return;
-
-		// this.authSubscription.add(
-		// 	this.authService.register(formValue).subscribe((response) => {
-		// 		console.log(response);
-		// 		// Navigate to Login Page after Successful Register
-		// 		this.router.navigate(["/login"]);
-		// 	})
-		// );
-	}
-
-	ngOnDestroy() {
-		this.authSubscription.unsubscribe();
-	}
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
 }
