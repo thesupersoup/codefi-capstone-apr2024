012105d7466af6321ee9e0e36d44b14bd6742349
Blake Graviett <blake.graviett@gmail.com>
2024-05-02 15:15:36 -0500

    rebase

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,40 +1,44 @@
-import { Component } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { AuthService } from "../../../shared/services/auth.service";
-import { Subscription } from "rxjs";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../../../shared/services/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
-	selector: "app-login",
-	templateUrl: "./login.component.html",
-	styleUrl: "./login.component.scss"
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-	// CREATE LOGIN FORM
-	loginForm = new FormGroup({
-		email: new FormControl("", [Validators.required, Validators.email]), // make sure it is a valid email address
-		password: new FormControl("", [Validators.required])
-	});
+  // CREATE LOGIN FORM
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]), // make sure it is a valid email address
+    password: new FormControl('', [Validators.required]),
+  });
 
-	private authSubscription = new Subscription();
+  private authSubscription = new Subscription();
 
-	constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-	// On Submit Function
-	onSubmit() {
-		if (this.loginForm.invalid) return;
-		const formValue = this.loginForm.getRawValue();
+  // On Submit Function
+  onSubmit() {
+    if (this.loginForm.invalid) return;
 
-		if (!formValue) return;
+    this.authSubscription.add(
+      this.authService
+        .loginUser(this.loginForm.value.email, this.loginForm.value.password)
+        .subscribe(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.error('Error:', error); // Handle error
+          }
+        )
+    );
+  }
 
-		// this.authSubscription.add(this.authService.login(formValue).subscribe((res: any) =>
-    //   {res = res.data.user.role;
-		// 		console.log(res);
-		// 	})
-		// );
-	}
-
-	ngOnDestroy() {
-		this.authSubscription.unsubscribe();
-	}
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
 }
