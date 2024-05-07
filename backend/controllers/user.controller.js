1b98c54984f1e7548dd4652253f1ec9a5a8dcf5f
Blake Graviett <blake.graviett@gmail.com>
2024-05-06 19:04:43 -0500

    FEAT: Get User Profile info backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 // * IMPORTS * //
 const User = require('../models/User.model')
+const Tag = require('../models/Tag.model')
 const { unsuccessfulRes, successfulRes } = require('../lib/utils/res')
 const cloudinary = require('cloudinary').v2
 const fs = require('fs')
@@ -45,9 +46,49 @@ const uploadSingleImageToCloudinary = async (req, res) => {
   successfulRes({ res, data: { src: result.secure_url } })
 }
 
+// Get All User Profile Data
+// TODO: User { Name, Email, Phone Number, Profile Picutre, Tags[] }
+const getAllUserDataById = async (req, res) => {
+  // get the user id from the req user
+  const userId = req.user.userId
+
+  // find the user by id
+  const user = await User.findById(userId)
+
+  // check if the user exists
+  if (!user) {
+    return unsuccessfulRes({
+      res,
+      message: 'User not found',
+    })
+  }
+
+  // get all the tag names
+  let tags = []
+  for (let tag of user.tags) {
+    const foundTag = await Tag.findById(tag)
+    console.log(foundTag)
+
+    tags.push(foundTag.name)
+  }
+
+  // return the users data
+  return successfulRes({
+    res,
+    data: {
+      name: user.firstName + ' ' + user.lastName,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      profilePicture: user.profilePic,
+      tags: tags,
+    },
+  })
+}
+
 // * EXPORTS * //
 
 module.exports = {
   updateUserProfile,
   uploadSingleImageToCloudinary,
+  getAllUserDataById,
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,12 +4,15 @@ const { authenticateUser } = require('../middleware/auth.middleware')
 const {
   updateUserProfile,
   uploadSingleImageToCloudinary,
+  getAllUserDataById,
 } = require('../controllers/user.controller')
 
 // * ROUTES  * //
 router.patch('/:id', authenticateUser, updateUserProfile)
 
 router.post('/upload-image', authenticateUser, uploadSingleImageToCloudinary)
+
+router.get('/', authenticateUser, getAllUserDataById)
 
 // * EXPORTS * //
 module.exports = router
