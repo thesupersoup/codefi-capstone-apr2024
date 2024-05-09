d6c4813e930cecff279dcd2e8a559ec0daf618d0
H20Beetle <waterbeetle091@gmail.com>
2024-05-09 15:51:15 -0500

    CHORE: COMMIT TO PULL NEW STAGING CHANGES

--- frontend/src/app/features/user-profile-page/user-profile-page.component.ts
+++ frontend/src/app/features/user-profile-page/user-profile-page.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Router } from 'express';
 
 @Component({
   selector: 'app-user-profile-page',
   templateUrl: './user-profile-page.component.html',
   styleUrls: ['./user-profile-page.component.scss'],
 })
-export class UserProfilePageComponent {}
-// CREATE USER PROFILE PAGE
+// export class UserProfilePageComponent {
+  // CREATE PROFILE FORM 1
+  // userProfileForm1 = new FormGroup({
+  //   img: new FormControl(''),
+  //   name: new FormControl('', [Validators.required]),
+  //   email: new FormControl('', [Validators.required, Validators.email]),
+  //   phone: new FormControl('', [Validators.required]),
+  //   role: new FormControl('', [Validators.required]),
+  // });
+
+  // CREATE PROFILE FORM 2
+//   userProfileForm2 = new FormGroup({
+//     tag1: new FormControl(''),
+//     tag2: new FormControl(''),
+//     tag3: new FormControl(''),
+//     tag4: new FormControl(''),
+//     tag5: new FormControl(''),
+//   });
+// }
 
 //TODO: add auto gen name/email/phone/role/profile pic pull userID from backend
 //TODO: add password change/reset logic
